cbf2bf94ed51f251625ecce9905a337af28b914a
Gustavo <gustavodarocha241@gmail.com>
2022-11-08 16:19:46 -0300

    requisitos 1 e 2 completos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,5 +16,24 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
-  
+});
+
+app.get('/talker', async (_req, res) => {
+  const pathName = path.resolve(__dirname, 'talker.json');
+  const talkers = JSON.parse(await fs.readFile(pathName, 'utf-8'));
+
+  res.status(HTTP_OK_STATUS).send(talkers)
+});
+
+app.get('/talker/:id',async (req, res) => {
+  const id = Number(req.params.id);
+  const pathName = path.resolve(__dirname, 'talker.json');
+  const talkers = JSON.parse(await fs.readFile(pathName, 'utf-8'));
+
+  const talker = talkers.find((t) => t.id === id);
+  if (talker) {
+    res.json(talker);
+  } else {
+    res.status(404).send({ message: "Pessoa palestrante não encontrada" });
+  }
 });
